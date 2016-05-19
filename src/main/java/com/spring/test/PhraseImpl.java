8377b09cb5cccbc0f41236bcf93c6be23941b31b
Borya Khripko <boris.katran@gmail.com>
2016-05-20 00:29:17 +0300

    New branch and new method 'init()'

--- src/main/java/com/spring/test/PhraseImpl.java
+++ src/main/java/com/spring/test/PhraseImpl.java
@@ -1,5 +1,7 @@
 package com.spring.test;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Created by Boris on 18.05.2016.
  */
@@ -9,6 +11,16 @@ public class PhraseImpl implements Phrase {
     private int repeat;
 
     private String phrase;
+
+    public PhraseImpl() {
+        System.out.println("Phase 1");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
 
     public void setPhrase(String phrase) {
         this.phrase = phrase;
